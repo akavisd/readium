08cae1cc55ba7af2cdaa777d3ee7a835fbd32a47
ISD <akav@isd.dp.ua>
2013-07-18 15:04:55 +0300

    Change remote service url

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,7 @@
 
-	window.REMOTE_SERVICE_URL = "http://localhost:8000";
+window.REMOTE_SERVICE_URL = "http://iakavarma.design.isd.dp.ua";
+
+//	window.REMOTE_SERVICE_URL = "http://localhost:8000";
 //  	window.REMOTE_SERVICE_URL = "http://bookreader.websolutions.dp.ua/base";
     window.EMPTY_COVER_HREF = "./images/library/missing-cover-image.png";
     
